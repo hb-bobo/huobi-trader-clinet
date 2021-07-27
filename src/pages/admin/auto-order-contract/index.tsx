87f77df19e941a79eca981e4b47644db7bcdea8d
hubo02 <hubo02@bilibili.com>
2021-07-27 17:37:25 +0800

    refactor: 合约倍数调整

--- src/pages/admin/auto-order-contract/index.tsx
+++ src/pages/admin/auto-order-contract/index.tsx
@@ -47,16 +47,16 @@ const periodOptions = [
 ];
 const leverRateOptions = [
   {
+    value: 2,
+    label: '2',
+  },
+  {
+    value: 3,
+    label: '3',
+  },
+  {
     value: 5,
     label: '5',
-  },
-  {
-    value: 10,
-    label: '10',
-  },
-  {
-    value: 20,
-    label: '20',
   },
 ];
 const AutoOrder: React.FC<Props> = props => {
